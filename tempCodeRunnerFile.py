7e33887d3033e5b15731e258946fa836d604a9ab
EunseokKang <es060604@gmail.com>
2023-09-07 15:22:49 +0900

    09.07

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,9 @@
-title
+
+    actions = ActionChains(dr)
+    actions.send_keys(Keys.ENTER).perform()
+    element = wait.until(EC.presence_of_element_located((By.ID, "divSttn")))
+
+    if a == '서구':
+        gu = dr.find_element(By.ID, "label_30170")
+        gu.click()
+    elif a == '중구':
